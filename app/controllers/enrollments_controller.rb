dc5bd9d21aed1aa6383edc09031f1b6c36e43e60
Lucas Sanches <sanches.lz@hotmail.com>
2021-03-24 23:11:12 -0300

    wip customer escolhe um plano

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -5,4 +5,28 @@ class EnrollmentsController < ApplicationController
     @plans = Plan.all
     @payment_methods = PaymentMethod.all
   end
+
+  def create
+    @enrollment = Enrollment.new(allowed_params)
+    if @enrollment.save
+      # TODO: enviar dados para api de pagamento
+      # TODO: enviar mensagem para sistema de aulas
+      redirect_to root_path
+    else
+      @customer = Customer.find_by(token: params[:token])
+      @plans = Plan.all
+      @payment_methods = PaymentMethod.all
+      render :new
+    end
+  end
+
+  private 
+
+  def allowed_params
+    {
+      customer: Customer.find_by(token: params[:customer]),
+      plan: Plan.find(params[:plan]),
+      payment_method: params[:method]
+    }
+  end
 end
--- spec/features/customer_choose_plan_spec.rb
+++ spec/features/customer_choose_plan_spec.rb
@@ -82,4 +82,23 @@ feature 'Customer enroll a plan' do
     expect(page).to have_content(plan.name)
     expect(page).to have_content(ccred.name)
   end
+
+  scenario 'fill the form' do
+    # Arrange
+    customer = create(:customer, token: '123')
+    yoga = create(:class_category, name: 'Yoga')
+    plan = create(:plan, name: 'PlanoFit', monthly_rate: 200, monthly_class_limit: 5)
+    create(:class_category_plan, plan: plan, class_category: yoga)
+    ccred = PaymentMethod.new(name: 'Cartão de Crédito', code: 'CCRED')
+    allow(PaymentMethod).to receive(:all).and_return([ccred])
+
+    # Act
+    visit new_enrollment_path(token: '123')
+    choose plan.name 
+    choose ccred.name
+    click_on 'Comprar Plano'
+
+    # Assert
+    expect(current_path).to eq(root_path)
+  end
 end
